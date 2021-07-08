ff06fc70e09b23f6f2c2b5a19c87a4c3bc1a16c4
Peter Li <peterleepersonal@gmail.com>
2021-07-08 12:39:56 -0400

    chore: yarn upgrade

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import commander from 'commander';
+import { program as commander } from 'commander';
 
 import { getNewPackageVersion } from './getNewPackageVersion';
 import { readPackageJson } from './readPackageJson';
